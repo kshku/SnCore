f297798b8e95eacead53634e9d0f806244014b5c
kshku <1211shree@gmail.com>
2026-06-11 09:52:40 +0530

    fix: correct test.c to match 3-param UTF conversion API

--- test/test.c
+++ test/test.c
@@ -1,9 +1,9 @@
 #include <sncore/utf.h>
 
 int main(void) {
-    if (sn_utf8_to_utf16("abc", 3, NULL, 0) == 0) return 1;
+    if (sn_utf8_to_utf16("abc", NULL, 0) == 0) return 1;
 
-    if (sn_utf16_to_utf8(L"abc", 3, NULL, 0) == 0) return 2;
+    if (sn_utf16_to_utf8(L"abc", NULL, 0) == 0) return 2;
 
     return 0;
 }
