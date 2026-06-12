ffd4f99a0719dd5ca172c6fd529204ab50b2714a
kshku <1211shree@gmail.com>
2026-06-12 11:45:38 +0530

    chore: remove ununsed directory

--- cmake/SnCoreConfig.cmake
+++
@@ -1,7 +0,0 @@
-if(NOT TARGET sncore)
-    add_library(sncore STATIC IMPORTED)
-    set_target_properties(sncore PROPERTIES
-        IMPORTED_LOCATION "${CMAKE_CURRENT_LIST_DIR}/../../lib/${CMAKE_STATIC_LIBRARY_PREFIX}sncore${CMAKE_STATIC_LIBRARY_SUFFIX}"
-        INTERFACE_INCLUDE_DIRECTORIES "${CMAKE_CURRENT_LIST_DIR}/../../core/include"
-    )
-endif()
