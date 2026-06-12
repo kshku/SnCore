2c311acfca14a5e12863a510fb6670e137a02393
kshku <1211shree@gmail.com>
2026-06-12 14:22:35 +0530

    feat: add SnMemoryAllocator type

--- core/include/sncore/types.h
+++ core/include/sncore/types.h
@@ -3,10 +3,19 @@
 #include <stddef.h>
 #include <stdint.h>
 
-typedef void *(*SnMemoryAllocateFn)(size_t size, void *user_data);
-typedef void *(*SnMemoryReallocateFn)(void *ptr, size_t new_size, void *user_data);
-typedef void (*SnMemoryFreeFn)(void *ptr, void *user_data);
-typedef void (*SnLockFn)(void *user_data);
-typedef void (*SnUnlockFn)(void *user_data);
-typedef uint64_t (*SnTimeNowFn)(void *user_data);
-typedef uint64_t (*SnThreadIdFn)(void *user_data);
+typedef void *(*SnMemoryAllocateFn)(void *data, uint64_t size, uint64_t align);
+typedef void *(*SnMemoryReallocateFn)(void *data, void *ptr, uint64_t new_size, uint64_t align);
+typedef void (*SnMemoryFreeFn)(void *data, void *ptr);
+
+typedef struct SnMemoryAllocator {
+    SnMemoryAllocateFn alloc;
+    SnMemoryReallocateFn realloc;
+    SnMemoryFreeFn free;
+    void *data;
+} SnMemoryAllocator;
+
+typedef void (*SnLockFn)(void *data);
+typedef void (*SnUnlockFn)(void *data);
+
+typedef uint64_t (*SnTimeNowFn)(void *data);
+typedef uint64_t (*SnThreadIdFn)(void *data);
